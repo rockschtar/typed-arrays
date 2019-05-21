804809dab342bfd0145220f909044801befaf866
Stefan Helmer <rockschtar@gmail.com>
2019-05-21 16:54:36 +0000

    Update ci/docker_install.sh

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,6 +8,9 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git wget curl zlib1g-dev libzip-dev -yqq --fix-missing apt-utils
+apt-get install -yqq git libmcrypt-dev libpq-dev libcurl4-gnutls-dev libicu-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev
+
+
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit-7.phar
@@ -20,5 +23,6 @@ chmod +x /usr/local/bin/phpunit
 docker-php-ext-install zip
 
 pecl install xdebug
+docker-php-ext-enable xdebug
 
 # Enable Xdebug
